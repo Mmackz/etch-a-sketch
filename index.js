c0650523a549f9c52b1eb84259f9464e78d1a87a
Mmackz <matthewmackenz@gmail.com>
2022-03-13 14:32:33 -0700

    🚧 setup color picker

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
+// DOM elements
 const drawingArea = document.getElementById("draw");
+const blackBtn = document.getElementById("black");
+const eraserBtn = document.getElementById("eraser");
+const clearBtn = document.getElementById("clear");
 
+// color picker using iro.js package
+const colorPicker = new iro.ColorPicker("#picker", {
+   width: 100,
+   layoutDirection: "horizontal",
+   sliderSize: 20,
+   handleRadius: 5,
+   borderWidth: 2,
+   borderColor: "black",
+   margin: 30
+});
+
+// set height of drawing area based off calulated width to keep 1:1 ratio.
 function setHeight() {
    const width = drawingArea.getBoundingClientRect().width;
    const height = width;
    drawingArea.style.height = height + "px";
 }
 
+// append squares inside the drawing area
+// percents are used to make the drawing area responsive
 function appendSquares(size) {
+   drawingArea.innerText = "";
    const sqWidth = 100 / size + "%";
    const sqHeight = sqWidth;
-   for (let i = 0; i < size**2; i++) {
+   for (let i = 0; i < size ** 2; i++) {
       const square = document.createElement("div");
+      square.classList.add("square");
       square.style.width = sqWidth;
       square.style.height = sqHeight;
       square.style.background = "blue";
@@ -18,14 +38,25 @@ function appendSquares(size) {
    }
 }
 
-// set initial height for mobiles on load
-setHeight()
+// set initial height on load
+setHeight();
+appendSquares(32);
 
-
-appendSquares(32)
-
-// set height for drawing area
+// set height for drawing area on resize
 window.addEventListener("resize", (event) => {
-   setHeight()
-   
+   setHeight();
 });
+
+
+// event listeners for buttons
+blackBtn.addEventListener("click", () => {
+   colorPicker.color.set("#000")
+});
+
+eraserBtn.addEventListener("click", () => {
+   colorPicker.color.set("#fff")
+})
+
+clearBtn.addEventListener("click", () => {
+   appendSquares(32);
+})
